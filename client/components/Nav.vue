57695224c0d1e537954118d6e0ba922e83d85355
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-09-28 21:34:38 +0600

    friendzy app by laravel and nuxtjs

---
+++ client/components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="bg-blue-500 text-white text-md">
+      <nav class="container flex justify-between items-center mx-auto py-5">
+         <span class="font-semibold text-2xl tracking-wide">Friendzy</span>
+         <div class="space-x-2">
+            <template v-if="this.$auth.loggedIn">
+               <span>Welcome,
+                  <nuxt-link to="#" class="capitalize hover:text-blue-200">{{ this.$auth.user.username }}</nuxt-link>
+               </span>
+               <nuxt-link to="/friends" class="hover:text-blue-200">FRIENDS</nuxt-link>
+               <nuxt-link to="/users" class="hover:text-blue-200">USERS</nuxt-link>
+               <nuxt-link to="/dashboard" class="hover:text-blue-200">DASHBOARD</nuxt-link>
+               <a href="#" @click.prevent="logout" class="hover:text-blue-200">LOGOUT</a>
+            </template>
+            <template v-else>
+               <nuxt-link to="/auth/register" class="hover:text-blue-200">REGISTER</nuxt-link>
+               <nuxt-link to="/auth/login" class="hover:text-blue-200">LOGIN</nuxt-link>
+            </template>
+         </div>
+      </nav>
+   </div>
+</template>
+
+<script>
+export default {
+   methods: {
+      logout() {
+         this.$auth.logout()
+      }
+   },
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+   <div class="container max-w-6xl mx-auto mt-48 sm:px-6 lg:px-8">
+      <div class="flex justify-center items-center bg-blue-200 h-56">
+         <span class="text-grey-700 text-5xl">Friendzy</span>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   middleware: 'guest',
+}
+</script>
